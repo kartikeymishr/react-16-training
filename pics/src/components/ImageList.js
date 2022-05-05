4cd9e4473b577790f16bd35a3dc87fd2a18a88a5
Kartikey Mishr <kartikeymishr@hotmail.com>
2022-05-06 02:11:56 +0530

    Displaying images in a fluid grid using React JSX.

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,13 +1,15 @@
+import './ImageList.css'
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-    const images = props.images.map(({description, id, urls}) => {
-        return <div key={id}>
-            <img src={urls.regular} alt={description}/>
+    const images = props.images.map((image) => {
+        return <div key={image.id}>
+            <ImageCard image={image}/>
         </div>
     })
 
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 }
 
 export default ImageList
