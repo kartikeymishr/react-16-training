036c0f437098faa1185ae3094f91a89adb4953b8
Kartikey Mishr <kartikeymishr@hotmail.com>
2022-05-06 01:14:50 +0530

    Showing images

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 const ImageList = (props) => {
-    console.log(props.images);
+    const images = props.images.map(({description, id, urls}) => {
+        return <div key={id}>
+            <img src={urls.regular} alt={description}/>
+        </div>
+    })
 
-    return <div>ImageList</div>
+    return <div>{images}</div>
 }
 
 export default ImageList
