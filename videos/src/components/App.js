42ae99531faae1735ad335cd5c990b3899c31983
Kartikey Mishr <kartikeymishr@hotmail.com>
2022-05-07 01:08:30 +0530

    Finishing touches.

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 import youtube from "../api/youtube";
 
 class App extends React.Component {
     state = {
         videos: [],
         selectedVideo: null
+    }
+
+    componentDidMount() {
+        this.onTermSubmit('donut media')
     }
 
     onTermSubmit = async (term) => {
@@ -16,17 +21,29 @@ class App extends React.Component {
             }
         })
 
-        this.setState({videos: response.data.items})
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        })
     }
 
     onVideoSelect = (video) => {
-        console.log('From App.js', video);
+        this.setState({selectedVideo: video})
     }
 
     render() {
         return <div className="ui container">
             <SearchBar onFormSubmit={this.onTermSubmit}/>
-            <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                        <VideoDetail video={this.state.selectedVideo}/>
+                    </div>
+                    <div className="five wide column">
+                        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
+                    </div>
+                </div>
+            </div>
         </div>
     }
 }
