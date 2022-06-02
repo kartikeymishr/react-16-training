1e862390296d5e27c0a160ac2d03e7064889b443
Kartikey Mishr <kartikeymishr@hotmail.com>
2022-06-02 19:30:17 +0530

    Adding timeout to search wikipedia API

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -22,12 +22,32 @@ const Search = () => {
             setResults(data.query.search)
         }
 
-        search()
-    }, [term])
+        if (term && !results.length) {
+            search()
+        } else {
+            const timeoutId = setTimeout(() => {
+                if (term) {
+                    search()
+                }
+            }, 500)
+
+            return () => {
+                clearTimeout(timeoutId)
+            }
+        }
+    }, [term, results.length])
 
     const renderedResults = results.map((result) => {
         return (
             <div className="item" key={result.pageid}>
+                <div className="right floated content">
+                    <a
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
+                </div>
                 <div className="content">
                     <div className="header">
                         {result.title}
