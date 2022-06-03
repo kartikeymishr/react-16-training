717dac66fd141740207f1a32c7ae72ecc3f20676
Kartikey Mishr <kartikeymishr@hotmail.com>
2022-06-03 20:56:40 +0530

    Cleaning up dropdown element.

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,7 +1,30 @@
-import React from "react"
+import React, {useEffect, useRef, useState} from "react"
 
 const Dropdown = ({options, selected, onSelectedChange}) => {
+    const [open, setOpen] = useState(false)
+    const ref = useRef()
+
+    useEffect(() => {
+        const onBodyClick = (event) => {
+            if (ref.current.contains(event.target)) {
+                return
+            }
+
+            setOpen(false)
+        }
+
+        document.body.addEventListener('click', onBodyClick, {capture: true})
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick, {capture: true})
+        }
+    }, [])
+
     const renderedOptions = options.map((option) => {
+        if (option.value === selected.value) {
+            return null
+        }
+
         return (
             <div
                 key={option.value}
@@ -13,17 +36,21 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
         )
     })
 
+    const active = open ? 'visible active' : '';
+    const transition = open ? 'visible transition' : '';
+
     return (
-        <div>
-            <div className="ui form">
-                <div className="field">
-                    <label className="label">Select a Colour</label>
-                    <div className="ui selection dropdown visible active">
-                        <i className="dropdown icon"/>
-                        <div className="text">{selected.label}</div>
-                        <div className="menu visible transition">
-                            {renderedOptions}
-                        </div>
+        <div ref={ref} className="ui form">
+            <div className="field">
+                <label className="label">Select a Colour</label>
+                <div
+                    onClick={() => setOpen(!open)}
+                    className={`ui selection dropdown ${active}`}
+                >
+                    <i className="dropdown icon"/>
+                    <div className="text">{selected.label}</div>
+                    <div className={`menu ${transition}`}>
+                        {renderedOptions}
                     </div>
                 </div>
             </div>
