4882517babbace38b7770cab6ee2f2331c76ec63
Kartikey Mishr <kartikeymishr@hotmail.com>
2022-06-03 19:26:25 +0530

    Dropdown component created

---
+++ widgets/src/components/Dropdown.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+const Dropdown = ({options, selected, onSelectedChange}) => {
+    const renderedOptions = options.map((option) => {
+        return (
+            <div
+                key={option.value}
+                className="item"
+                onClick={() => onSelectedChange(option)}
+            >
+                {option.label}
+            </div>
+        )
+    })
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label className="label">Select a Colour</label>
+                    <div className="ui selection dropdown visible active">
+                        <i className="dropdown icon"/>
+                        <div className="text">{selected.label}</div>
+                        <div className="menu visible transition">
+                            {renderedOptions}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Dropdown
